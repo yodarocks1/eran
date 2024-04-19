7eba5b2399e7849186cf4ecfb219541a6eb5a47b
Bennett DenBleyker <bennett.denbleyker@usu.edu>
2024-04-19 00:29:31 -0600

    Fix symbolic links

--- install.sh
+++ install.sh
@@ -112,8 +112,8 @@ ldconfig
 
 touch ELINA/python_interface/__init__.py
 mkdir python_interface
-ln -s ELINA/python_interface python_interface/elina
-ln -s tf_verify python_interface/eran
+ln -s ./ELINA/python_interface ./python_interface/elina
+ln -s ./tf_verify ./python_interface/eran
 
 echo "To use ERAN, add '$ERAN_INSTALL_LOCATION/python_interface/' to your \$PYTHONPATH, or add the following lines to the top of your python file:"
 echo " import sys"
